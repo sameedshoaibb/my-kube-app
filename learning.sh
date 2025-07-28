0ad67fad0f8c06ca9c83182132e1f7625da2ccf2
sameedshoaibb <sameedshoaib@live.co.uk>
2025-07-28 14:31:50 +0200

    Restructuring the project

--- learning.sh
+++
@@ -1,5 +0,0 @@
-# Loki:
-
-Promtail is use to collect logs from the server, and will stream to Loki. We will get the logs, which will have label as a metadata that will have a index.
-We can use local filesystem to store the log, as well as to the cloud.
-We will be using LogQL to get the data from Loki.
